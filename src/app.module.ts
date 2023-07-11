fd10744c288a48ba7ca06e7e6af186922e1f7f1e
PP6464 <panthhp@outlook.com>
2023-07-11 15:20:34 +0100

    move configmodule to firebase.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DeleteShiftController } from './delete-shift/delete-shift.controller';
-import { ConfigModule } from '@nestjs/config';
 
-ConfigModule.forRoot();
 @Module({
   imports: [],
   controllers: [AppController, DeleteShiftController],
