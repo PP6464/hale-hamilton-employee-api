82df7ddc8b853225b3f49d267e3e82dfb37bd3ca
PP6464 <panthhp@outlook.com>
2023-07-12 11:49:32 +0100

    addd chat controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShiftController } from './shift/shift.controller';
+import { ChatController } from './chat/chat.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ShiftController],
+  controllers: [AppController, ShiftController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
