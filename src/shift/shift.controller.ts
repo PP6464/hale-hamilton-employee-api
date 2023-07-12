09296ca07a17d2b8e0be0a85695042d59523e503
PP6464 <panthhp@outlook.com>
2023-07-12 09:11:18 +0100

    reschedule a shift

--- src/shift/shift.controller.ts
+++ src/shift/shift.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Delete, Param, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  Patch,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { cloud_firestore, fcm } from '../firebase/firebase';
 import { DocumentReference } from 'firebase-admin/lib/firestore';
 import { firestore } from 'firebase-admin';
@@ -116,8 +124,8 @@ export class ShiftController {
       title: 'Shift addition',
       body: `Employee ${employee.id} (Name: ${
         employee.data()['name']
-      }, Email: ${employee.data()['email']}) has had a ${
-        shiftDetails.time
+      }, Email: ${employee.data()['email']}) has had ${
+        shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
       } shift added at ${shiftDetails.date
         .split('-')
         .reverse()
@@ -128,8 +136,8 @@ export class ShiftController {
     await cloud_firestore.collection('notifications').add({
       users: [employee.ref],
       title: 'Shift addition',
-      body: `You've been added to a ${
-        shiftDetails.time
+      body: `You've been added to ${
+        shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
       } shift by an administrator on ${shiftDetails.date
         .split('-')
         .reverse()
@@ -141,8 +149,8 @@ export class ShiftController {
         title: 'Shift addition',
         body: `Employee ${employee.id} (Name: ${
           employee.data()['name']
-        }, Email: ${employee.data()['email']}) has had a ${
-          shiftDetails.time
+        }, Email: ${employee.data()['email']}) has had ${
+          shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
         } shift added at ${shiftDetails.date
           .split('-')
           .reverse()
@@ -155,8 +163,8 @@ export class ShiftController {
       tokens: employee.data()['tokens'],
       notification: {
         title: 'Shift addition',
-        body: `You've been added to a ${
-          shiftDetails.time
+        body: `You've been added to ${
+          shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
         } shift by an administrator on ${shiftDetails.date
           .split('-')
           .reverse()
@@ -164,6 +172,108 @@ export class ShiftController {
       },
     });
     return 'Shift added';
+  }
+
+  @Patch('update/:id')
+  async updateShift(
+    @Param('id') id: string,
+    @Query('admin') admin: string,
+    @Body() shiftDetails: ShiftDetails,
+  ) {
+    const oldShift = await cloud_firestore.doc(`shifts/${id}`).get();
+    await cloud_firestore.doc(`shifts/${id}`).set({
+      employee: cloud_firestore.doc(`users/${shiftDetails.employee}`),
+      time: shiftDetails.time,
+      date: shiftDetails.date,
+    });
+    await cloud_firestore.collection(`shifts/${id}`).add({
+      type: 'PATCH',
+      shiftTime: shiftDetails.time,
+      shiftDate: shiftDetails.date,
+      oldTime: oldShift.data()['time'],
+      oldDate: oldShift.data()['date'],
+      administrator: cloud_firestore.doc(`users/${admin}`),
+      employee: cloud_firestore.doc(`users/${shiftDetails.employee}`),
+      timestamp: FieldValue.serverTimestamp(),
+    });
+    const employee = await cloud_firestore
+      .doc(`users/${shiftDetails.employee}`)
+      .get();
+    const admins = await cloud_firestore
+      .collection('users')
+      .where('isAdmin', '==', true)
+      .get();
+    await cloud_firestore.collection('notifications').add({
+      users: admins.docs.map((e) => e.ref),
+      title: 'Shift rescheduling',
+      body: `Employee ${employee.id} (Name: ${
+        employee.data()['name']
+      }, Email: ${employee.data()['email']}) has had ${
+        oldShift.data()['time'] === 'morning' ? 'a morning' : 'an evening'
+      } shift at ${oldShift
+        .data()
+        ['date'].split('-')
+        .reverse()
+        .join('/')} rescheduled to ${
+        shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
+      } shift by administrator ${
+        admins.docs.filter((e) => e.id === admin)[0].data()['name']
+      }`,
+    });
+    await cloud_firestore.collection('notifications').add({
+      users: [employee.ref],
+      title: 'Shift rescheduling',
+      body: `Your ${
+        oldShift.data()['time'] === 'morning' ? 'a morning' : 'an evening'
+      } shift on ${oldShift
+        .data()
+        ['date'].split('-')
+        .reverse()
+        .join('/')} has been rescheduled to ${
+        shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
+      } shift by an administrator on ${shiftDetails.date
+        .split('-')
+        .reverse()
+        .join('/')}.`,
+    });
+    await fcm.sendEachForMulticast({
+      tokens: admins.docs.flatMap((e) => e.data()['tokens'] as string[]),
+      notification: {
+        title: 'Shift rescheduling',
+        body: `Employee ${employee.id} (Name: ${
+          employee.data()['name']
+        }, Email: ${employee.data()['email']}) has had a ${
+          oldShift.data()['time']
+        } shift at ${oldShift
+          .data()
+          ['date'].split('-')
+          .reverse()
+          .join('/')} rescheduled to ${
+          shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
+        } shift by administrator ${
+          admins.docs.filter((e) => e.id === admin)[0].data()['name']
+        }`,
+      },
+    });
+    await fcm.sendEachForMulticast({
+      tokens: employee.data()['tokens'],
+      notification: {
+        title: 'Shift rescheduling',
+        body: `Your ${
+          oldShift.data()['time'] === 'morning' ? 'a morning' : 'an evening'
+        } shift on ${oldShift
+          .data()
+          ['date'].split('-')
+          .reverse()
+          .join('/')} has been rescheduled to ${
+          shiftDetails.time === 'morning' ? 'a morning' : 'an evening'
+        } shift by an administrator on ${shiftDetails.date
+          .split('-')
+          .reverse()
+          .join('/')}.`,
+      },
+    });
+    return 'Shift rescheduled';
   }
 }
 
