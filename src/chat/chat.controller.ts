408343275f9166032939ecce1aebc6fcec768c2a
PP6464 <panthhp@outlook.com>
2023-07-12 14:32:02 +0100

    change group users

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -8,18 +8,27 @@ import {
   Param,
 } from '@nestjs/common';
 import { cloud_firestore, fcm } from '../firebase/firebase';
-import { FieldValue } from 'firebase-admin/lib/firestore';
+import { DocumentReference, FieldValue } from 'firebase-admin/lib/firestore';
+import { firestore } from 'firebase-admin';
+import DocumentSnapshot = firestore.DocumentSnapshot;
 
 @Controller('chat')
 export class ChatController {
   @Put('group/new')
   async newGroup(@Query('name') name: string, @Body() members: string[]) {
-    const membersData = [];
+    const membersData: DocumentSnapshot[] = [];
     for (let i = 0; i < members.length; i++) {
       const member = await cloud_firestore.doc(`users/${members[i]}`).get();
-      if (!member.exists) return 'One of the members does not exist';
-      else membersData.concat(member);
+      if (!member.exists) {
+        return 'One of the members does not exist';
+      } else {
+        membersData.push(member);
+      }
     }
+    await cloud_firestore.collection('groups').add({
+      name: name,
+      users: membersData.map((e) => e.ref),
+    });
     await cloud_firestore.collection('notifications').add({
       users: members.map((e) => cloud_firestore.doc(`users/${e}`)),
       title: 'Addition into new group',
@@ -36,12 +45,74 @@ export class ChatController {
   }
 
   @Patch('group/:id/change')
-  async updateGroup(
+  async changeGroupUsers(
     @Param('id') id: string,
     @Query('type') type: 'add' | 'remove',
     @Query('user') user: string,
     @Query('by') by: string,
-  ) {}
+  ) {
+    const group = await cloud_firestore.doc(`groups/${id}`).get();
+    if (!group.exists) return 'Group does not exist';
+    const userChanged = await cloud_firestore.doc(`users/${user}`).get();
+    if (
+      !userChanged.exists ||
+      ((group.data()['users'] as DocumentReference[]).includes(
+        userChanged.ref,
+      ) &&
+        type === 'add') ||
+      (!(group.data()['users'] as DocumentReference[]).includes(
+        userChanged.ref,
+      ) &&
+        type === 'remove')
+    )
+      return 'User is invalid';
+    const userBy = await cloud_firestore.doc(`users/${by}`).get();
+    if (!userBy.exists || group.data()['users'].includes(userBy.ref))
+      return 'User by is invalid';
+    await group.ref.update({
+      users:
+        type === 'add'
+          ? FieldValue.arrayUnion(userChanged.ref)
+          : FieldValue.arrayRemove(userChanged.ref),
+    });
+    await cloud_firestore.collection('notifications').add({
+      users: (group.data()['users'] as DocumentReference[]).filter(
+        (e) => e.id !== userChanged.id,
+      ),
+      title: `${
+        type === 'add' ? 'Addition of user to' : 'Removal of user from'
+      } group`,
+      body:
+        type === 'add'
+          ? `Welcome ${userChanged.data()['name']} to the group ${
+              group.data()['name']
+            }`
+          : `${userChanged.data()['name']} was removed from the group ${
+              group.data()['name']
+            }`,
+      time: FieldValue.serverTimestamp(),
+    });
+    await cloud_firestore.collection('notifications').add({
+      users: [userChanged.ref],
+      title: `${type === 'add' ? 'Addition to' : 'Removal from'} group`,
+      body: `You have been ${type === 'add' ? 'added to' : 'removed from'} group ${group.data()['name']}`,
+      time: FieldValue.serverTimestamp(),
+    });
+    await fcm.sendEachForMulticast({
+      tokens: [],
+      notification: {
+        title: `${type === 'add' ? 'Addition' : 'Removal'} of user from group`,
+        body:
+          type === 'add'
+            ? `Welcome ${userChanged.data()['name']} to the group ${
+                group.data()['name']
+              }`
+            : `${userChanged.data()['name']} was removed from the group ${
+                group.data()['name']
+              }`,
+      },
+    });
+  }
 
   @Delete('group/:id')
   async deleteGroup(@Param('id') id: string, @Query('by') by: string) {}
