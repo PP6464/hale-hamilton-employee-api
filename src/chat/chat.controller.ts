b6ec5cdc700f381696c7e7581ee9d049f04138e9
PP6464 <panthhp@outlook.com>
2023-07-13 16:02:31 +0100

    formatting

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -16,7 +16,11 @@ import FieldValue = firebase.firestore.FieldValue;
 @Controller('chat')
 export class ChatController {
   @Put('group/new')
-  async newGroup(@Query('name') name: string, @Query('desc') desc: string, @Body() members: string[]) {
+  async newGroup(
+    @Query('name') name: string,
+    @Query('desc') desc: string,
+    @Body() members: string[],
+  ) {
     const membersData: DocumentSnapshot[] = [];
     for (let i = 0; i < members.length; i++) {
       const member = await cloud_firestore.doc(`users/${members[i]}`).get();
