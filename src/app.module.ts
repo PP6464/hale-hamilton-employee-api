51daffda07a80e626cdd176aafdb1f237778384a
PP6464 <panthhp@outlook.com>
2023-07-17 17:27:47 +0100

    Add auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ShiftController } from './shift/shift.controller';
 import { ChatController } from './chat/chat.controller';
+import { AuthController } from './auth/auth.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ShiftController, ChatController],
+  controllers: [AppController, ShiftController, ChatController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
