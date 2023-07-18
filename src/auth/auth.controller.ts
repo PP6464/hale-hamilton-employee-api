c6aedd48a1d9d13893c70eba260bfe750ce494b3
PP6464 <panthhp@outlook.com>
2023-07-18 14:38:33 +0100

    delete and reset employee password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Query, Put, Patch, Param } from '@nestjs/common';
+import { Body, Controller, Query, Put, Patch, Param, Delete } from '@nestjs/common';
 import { firestore, auth, fcm } from '../firebase/firebase';
+import { FieldValue } from 'firebase-admin/firestore';
 
 @Controller('auth')
 export class AuthController {
@@ -45,6 +46,7 @@ export class AuthController {
       users: [user.ref],
       title: 'Password reset',
       body: `Your password has been reset to ${pwd} by your department administrator`,
+      time: FieldValue.serverTimestamp(),
     });
     try {
       await fcm.sendEachForMulticast({
@@ -57,6 +59,42 @@ export class AuthController {
     } catch {
       return 'Notification sending failed but update completed';
     }
+  }
+
+  @Delete('employee/:uid')
+  async deleteEmployee(
+    @Param('uid') uid: string,
+    @Query('by') by: string,
+  ) {
+    const user = await firestore.doc(`users/${uid}`).get();
+    const adminBy = await firestore.doc(`users/${by}`).get();
+    if (!user.exists || user.data()['isAdmin']) return 'Invalid user';
+    if (!adminBy.exists || !adminBy.data()['isAdmin']) return 'Invalid admin';
+    const shifts = await firestore.collection('shifts').where('employee', '==', user.ref).get();
+    for (let shift of shifts.docs) {
+      await shift.ref.delete();
+    }
+    const admins = await firestore.collection('users').where('isAdmin', '==', true).get();
+    await firestore.collection('notifications').add({
+      users: [admins.docs.map((e) => e.ref)],
+      title: 'Employee account deletion',
+      body: `Employee ${uid} (Name: ${user.data()['name']}, Email: ${user.data()['email']}) has had their account deleted by administrator ${adminBy.data()['name']}`,
+      time: FieldValue.serverTimestamp(),
+    });
+    await auth.deleteUser(uid);
+    await user.ref.delete();
+    try {
+      await fcm.sendEachForMulticast({
+        tokens: admins.docs.flatMap((e) => e.data()['tokens']),
+        notification: {
+          title: 'Employee account deletion',
+          body: `Employee ${uid} (Name: ${user.data()['name']}, Email: ${user.data()['email']}) has had their account deleted by administrator ${adminBy.data()['name']}`,
+        }
+      });
+    } catch {
+      return 'Notification sending failed but update completed';
+    }
+    return 'Employee account deleted';
   }
 }
 
